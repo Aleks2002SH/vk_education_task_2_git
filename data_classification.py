a40fd0d21ef92857c43ff01b3349a2220019cafb
Sharonova_Aleksandra <aleksandra@admin.>
2024-11-25 22:59:53 +0300

    studying data

--- data_classification.py
+++ data_classification.py
@@ -17,3 +17,15 @@ print(np.sum(test_mask_missing_values_only))
 cols = list(train_df.columns)
 X = train_df.drop([cols[-1]],axis=1)
 y = train_df[cols[-1]]
+def find_non_numerical(df):
+    non_numerical_cols = df.select_dtypes(exclude=['number']).columns
+    if non_numerical_cols.empty:
+        print("All columns are numerical.")
+    else:
+        print(f"Non-numerical columns found: {list(non_numerical_cols)}")
+find_non_numerical(train_df)
+for col in cols[:-1]:
+    number_of_unique_values = len(X[col].unique())
+    if number_of_unique_values<X.shape[0]:
+        print(f'Columns - {col}, number of unique values - {number_of_unique_values}')
+print(f'Unique classes - {y.unique()}')
